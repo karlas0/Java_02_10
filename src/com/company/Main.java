aa1fa8bd9f567a39ea0eee6681e12f1186830c76
Karlas <karolis.zagarnauskas@gmail.com>
2018-01-30 16:58:40 +0200

    first commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,21 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+	System.out.println("Iveskite du skaicius");
+
+        Scanner sc = new Scanner(System.in);
+
+        float a = sc.nextFloat();
+        float b = sc.nextFloat();
+
+        System.out.println("Suma" + skaiciuotuvas.suma(a,b));
+        System.out.println("Skirtumas" + skaiciuotuvas.skirtumas(a,b));
+        System.out.println("Sandauga" + skaiciuotuvas.sandauga(a,b));
+
     }
 }
---
+++ src/com/company/skaiciuotuvas.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class skaiciuotuvas {
+    public static float suma(float a, float b) {
+        return a + b;
+    }
+
+    public static float skirtumas(float a, float b) {
+        return a - b;
+    }
+
+    public static float sandauga(float a, float b) {
+        return a * b;
+    }
+}
+
+
